dadfb4c5a37de71e6da5d82d06931eb1ddc7fe59
Ashelynne Juniper <ashe.juniper@pm.me>
2023-10-17 20:00:48 -0500

    Prerelease 0.1.1-alpha.2
    Added
    * Improve build command
    * Add proper binary build directory
    * Add `prepublishOnly` script to auto-generate `.zip` files containing all distributable binary executables

--- scripts/dist/rename.sh
+++ scripts/dist/rename.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Rename executables.
-mv dist/$1-linux    dist/$1-$2-linux-x64
-mv dist/$1-macos    dist/$1-$2-macos-x64
-mv dist/$1-win.exe  dist/$1-$2-win32-x64.exe
+mv dist/bin/$1-linux    dist/bin/$1-$2-linux-x64
+mv dist/bin/$1-macos    dist/bin/$1-$2-macos-x64
+mv dist/bin/$1-win.exe  dist/bin/$1-$2-win32-x64.exe
--- scripts/dist/zip.sh
+++ scripts/dist/zip.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+rm -rf dist/release/
+
 # Zip executables for distribution.
-zip -9 dist/$1-$2-linux-x64.zip dist/$1-$2-linux-x64
-zip -9 dist/$1-$2-macos-x64.zip dist/$1-$2-macos-x64
-zip -9 dist/$1-$2-win32-x64.zip dist/$1-$2-win32-x64.exe
+zip -9 dist/release/$1-$2-linux-x64.zip dist/bin/$1-$2-linux-x64
+zip -9 dist/release/$1-$2-macos-x64.zip dist/bin/$1-$2-macos-x64
+zip -9 dist/release/$1-$2-win32-x64.zip dist/bin/$1-$2-win32-x64.exe
